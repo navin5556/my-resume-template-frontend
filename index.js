cb78f1330c54a2da08497a4fa8cbba21c76526b3
Naveen Kumar <navin5556@gmail.com>
2024-07-04 21:30:11 +0530

    updated terraform url

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://br6uutkpoll2mi3c3h3xgu52z40uimoh.lambda-url.ap-south-1.on.aws/"
+        "https://ziowtiehth6ppe5i5krknpjdhy0wstub.lambda-url.ap-south-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`; //updating the value in index.html
